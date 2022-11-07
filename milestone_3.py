b4732a3157bc248eab0f04c89507d380502ce9fb
Charles Feinson <charlie@feinson.com>
2022-11-07 22:52:49 +0000

    milestone 3 task 2

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,15 @@
+import random
+word_list = ["raspberry","strawberry","blueberry","banana","guava"]
+word = random.choice(word_list)
+
 while True:
-    guess = input("Guess a letter")
+    guess = (input("Guess a letter\n")).lower()
     if len(guess)==1 and guess.isalpha():
         break
     else:
         "Invalid letter. Please, enter a single alphabetical character."
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
