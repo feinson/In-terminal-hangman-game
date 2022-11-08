5b0a2a940aaab1c1ec64c683cde5006c46ab45b7
Charles Feinson <charlie@feinson.com>
2022-11-08 12:59:28 +0000

    just completing some practicals don't mind me

---
+++ practical.py
@@ -0,0 +1,16 @@
+# clothing = {"type": "dress", "price": "£30", "colour": "red"}
+
+# def apparel_describer(dicky_wicky, attributes_to_print="all"):
+#     if attributes_to_print == "all":
+#         attributes_to_print=list(dicky_wicky.keys())
+#         final=[]
+#     for item in dicky_wicky:
+#         if item in attributes_to_print:
+#             print(f"{item} : {dicky_wicky[item]}")
+#     print(len(attributes_to_print))
+#     return "thats it"
+
+
+
+# print(apparel_describer(clothing))
+
