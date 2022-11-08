b96b506507affdc25c62a079cc0c24dd932f5099
Charles Feinson <charlie@feinson.com>
2022-11-08 20:29:50 +0000

    attempted to initialise things in the Hangman

--- milestone_4.py
+++ milestone_4.py
@@ -7,4 +7,15 @@ class Hangman:
         self.num_lives = num_lives 
         self.word = random.choice(word_list)
         self.word_guessed = ['_'] * len(self.word)
-        
+
+        char_list = []
+        char_list[:0] = self.word
+        char_set = set(char_list)
+        self.num_letters=len(char_set)
+
+
+        self.word_list = word_list
+        self.list_of_guesses = []
+
+
+x=Hangman(my_word_list)
