0e9f24635b6f959764eb136d2633593b4b8a9b39
Charles Feinson <charlie@feinson.com>
2022-11-08 22:01:09 +0000

    edited the check_guess function

--- milestone_4.py
+++ milestone_4.py
@@ -7,19 +7,17 @@ class Hangman:
         self.num_lives = num_lives 
         self.word = random.choice(word_list)
         self.word_guessed = ['_'] * len(self.word)
-
-        char_list = []
-        char_list[:0] = self.word
-        char_set = set(char_list)
-        self.num_letters=len(char_set)
-
-
+        self.num_letters = len(set(self.word))
         self.word_list = word_list
         self.list_of_guesses = []
 
     def check_guess(self, guess):
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
+            index=[pos for pos, char in enumerate(self.word) if char == guess]
+            for i in index:
+                self.word_guessed[i]=guess
+            print(self.word_guessed)
         else:
             print(f"Sorry, {guess} is not in the word. Try again.")
 
@@ -38,3 +36,8 @@ class Hangman:
     
 x=Hangman(my_word_list)
 x.ask_for_input()
+
+
+# s = 'python is fun'
+# c = 'n'
+# print([pos for pos, char in enumerate(s) if char == c])
